ca9129c3ca14aacf0260ee39364478b137885a69
André Freitas <amfreitas23@ua.pt>
2024-11-06 23:21:40 +0000

    Menu Test

---
+++ Assets/Scripts/MenuFreature.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFreature : MonoBehaviour
+{
+    [SerializeField] private GameObject menu;
+    [SerializeField] private OVRInput.Button buttonMenu;
+
+    private void Awake() => menu.SetActive(false);
+
+    private void Update()
+    {
+        if (OVRInput.GetDown(buttonMenu))
+        {
+            menu.SetActive(!menu.activeSelf);
+        }
+    }
+}
